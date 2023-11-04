ebddeb8c347a1ee600ecacb63e79c84e0eaab12e
Ponomarev Ivan <i.ponomarev.1991@gmail.com>
2023-11-04 22:48:10 +0300

    fixed checkstyle#2

--- src/main/java/edu/hw4/Task11.java
+++ src/main/java/edu/hw4/Task11.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Task11 {
 
     private static final int LIMITATION = 100;
+
     public List<Animal> getTallAndBiting(List<Animal> animals) {
         return animals.stream().filter(animal -> animal.bites() && animal.height() > LIMITATION).toList();
     }
